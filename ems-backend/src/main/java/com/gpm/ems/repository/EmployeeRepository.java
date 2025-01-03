88d72fd0d64c7e05b7da3cfe900fc6b0945f50dc
poojagudale <poojagudale217@gmail.com>
2025-01-03 22:28:44 +0530

    Add files via upload

---
+++ ems-backend/src/main/java/com/gpm/ems/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.gpm.ems.repository;
+
+import com.gpm.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
